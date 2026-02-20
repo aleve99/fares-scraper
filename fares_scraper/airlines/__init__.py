c70b177f9373bc007323b9d22f0919002ae37a73
aleve99 <alessiovecchiet@gmail.com>
2026-02-20 21:34:04 +0100

    Updated version

--- fares_scraper/airlines/__init__.py
+++ fares_scraper/airlines/__init__.py
@@ -1,4 +1,5 @@
 from .ryanair import RyanairScraper
 from .wizzair import WizzAirScraper
+from .iberia import IberiaScraper
 
-__all__ = ["RyanairScraper", "WizzAirScraper"]
+__all__ = ["RyanairScraper", "WizzAirScraper", "IberiaScraper"]
