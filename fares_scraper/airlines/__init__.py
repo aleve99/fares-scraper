879ad6d37fc34e5d90ef241627defd0a82c0bc74
Alessio <93376666+aleve99@users.noreply.github.com>
2026-06-29 12:06:47 +0200

    feat: register LotScraper in airlines __init__

--- fares_scraper/airlines/__init__.py
+++ fares_scraper/airlines/__init__.py
@@ -3,5 +3,13 @@ from .wizzair import WizzAirScraper
 from .iberia import IberiaScraper
 from .volotea import VoloteaScraper
 from .easyjet import EasyJetScraper
+from .lot import LotScraper
 
-__all__ = ["RyanairScraper", "WizzAirScraper", "IberiaScraper", "VoloteaScraper", "EasyJetScraper"]
+__all__ = [
+    "RyanairScraper",
+    "WizzAirScraper",
+    "IberiaScraper",
+    "VoloteaScraper",
+    "EasyJetScraper",
+    "LotScraper",
+]
